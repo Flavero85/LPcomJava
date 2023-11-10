0da67bcc39d9131e05b962c6c5143a92c4edaaf4
FLAVIO RAFAEL CARDOZO <flavero@gmail.com>
2023-11-10 19:45:59 -0300

    up

---
+++ aula02/exercicio08.js
@@ -0,0 +1,21 @@
+//8. Faça um Programa que leia um número e exiba o dia correspondente da semana. (1-Domingo, 2- Segunda, etc.), se digitar outro valor deve aparecer valor inválido.
+
+let dias_semana
+dias_semana = 7
+
+
+if(dias_semana == 1){
+    console.log("Segunda-Feira")
+}else if(dias_semana == 2){
+    console.log("Terça-Feria")
+}else if(dias_semana == 3){
+    console.log("Quarta-Feira")
+}else if(dias_semana == 4){
+    console.log("Quinta-Feria")
+}else if(dias_semana == 5){
+    console.log("Sexta-Feira")
+}else if(dias_semana == 6){
+    console.log("Sabado")
+}else if(dias_semana == 7){
+    console.log("Domingo")
+}
---
+++ aula02/exercicio09.js
@@ -0,0 +1,8 @@
+//9. Faça um Programa que peça um número inteiro e determine se ele é par ou impar. Dica: utilize o operador módulo (resto da divisão).
+
+let numero
+numero = 2
+
+if(numero % 0 ){
+    console.log("numero par")
+}
